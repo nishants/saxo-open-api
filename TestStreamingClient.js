856da3d438560ab9dbf04a35b765b6c81679ef18
nishant <nishant.singh87@gmail.com>
2021-07-07 20:54:36 +0530

    Create a ndoe scriopt for signalr

--- TestStreamingClient.js
+++ TestStreamingClient.js
@@ -1,3 +1,57 @@
-const StreamingClient = require('./src/StreamingClient');
+const signalR = require('@microsoft/signalr')
 
-StreamingClient.creatConnection();
+const start = () => {
+  let pricingConnection ;
+
+  const showMessage = (content) => {
+    console.log('message : ', content)
+  };
+
+  const subscribeToListener = async () => {
+    pricingConnection.stream("StartStreaming")
+      .subscribe({
+        next: showMessage,
+        error: showMessage,
+      });
+  };
+
+
+  const creatConnection = async ({token, onSuccess, streamingServerUrl, contextId }) => {
+    pricingConnection = new signalR.HubConnectionBuilder()
+      .withUrl(`${streamingServerUrl}?contextId=${contextId}`, { accessTokenFactory: () => token })
+      .build();
+    try{
+      await pricingConnection.start();
+      showMessage("Conncted with server");
+      await subscribeToListener();
+      onSuccess();
+    }catch(err){
+      showMessage("Failed to connect to server" + err.toString());
+    }
+  }
+
+  const stop = async () => {
+    await pricingConnection.stop();
+  }
+
+  function connect(){
+    const token = '';
+    const contextId = '';
+    const streamingServerUrl = '';
+
+    creatConnection({token, contextId, streamingServerUrl, onSuccess: () => updateUi({connected: true})});
+  }
+
+  function disconnect (){
+    stop().then(() => console.log("Disconnected"));
+  }
+
+  connect();
+
+  setTimeout(10000, () => {
+    console.log("Trying to disconnect");
+    disconnect();
+  })
+}
+
+start();
